2725b7c43155f985f6e5e4d4a8347d6e4c1e02c2
arock475 <aaronjspears01@gmail.com>
2023-10-14 12:06:33 -0400

    Added basic hangman functionality as well as best 2 / 3

--- HangMan/settings.py
+++ HangMan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'HangMan',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'HangMan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'HangMan/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +119,25 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    }
+}
---
+++ HangMan/views.py
@@ -0,0 +1,84 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import string, logging, requests
+#configuration value
+NUMGUESSES = 6
+
+def newGame(request):
+    logger = logging.getLogger('django')
+    wins = 0
+    games = 0
+    if request.method == 'GET':
+        # save current number of wins and games played for state
+        if 'wins' in request.session.keys():
+            wins = request.session['wins']
+        if 'games' in request.session.keys():
+            games = request.session['games']
+        # if win best 2 out of 3 send to final win
+        if games >= 2 and wins > 1:
+            request.session['wins'] = 0
+            request.session['games'] = 0
+            return render(request, "finalWin.html")
+        # if lose best 2 out of 3 send to final lose
+        elif (games == 3 and wins <= 1) or (games == 2 and wins == 0):
+            request.session['wins'] = 0
+            request.session['games'] = 0
+            return render(request, "finalLose.html")
+        # else continue the games
+        request.session.flush()
+        request.session['wins'] = wins
+        request.session['games'] = games
+        # send list of letters to make buttons
+        alph = list(string.ascii_lowercase)
+        request.session['alph'] = alph
+        # store answer from random word api
+        # answer = requests.get("https://random-word-api.herokuapp.com/word").text
+        # answer = answer[2:len(answer) - 2]
+        answer = 'test'
+        # logger.info(answer[2:len(answer) - 2])
+        request.session['answer'] = answer
+        # store record of guessed answers
+        request.session['guessed'] = []
+        # store record of number of incorrect guesses made
+        request.session['guessNum'] = 0
+        # store record of current guess
+        request.session['cur_guess'] = ''
+        # store record of number correct guesses
+        request.session['correct'] = 0
+        # store record of previous guess
+        request.session['prev_guess'] = ''
+        # store game status
+        request.session['status'] = 1
+        return render(request, "mainPage.html")
+    else:
+        return buttonPress(request)
+
+
+def buttonPress(request):
+    logger = logging.getLogger('django')
+    # get the guessed letter from the request
+    cur_guess = request.POST['letter']
+    # put guessed letter into guessed list
+    guessed = request.session['guessed']
+    guessed.append(cur_guess)
+    request.session['guessed'] = guessed
+    # get the answer from the request
+    answer = request.session['answer']
+    # get the previous guess from the request
+    prev = request.session['prev_guess']
+    # prevents guess incrementing due to refreshing the browser
+    if cur_guess != prev:
+        # increment correct counter
+        if cur_guess in answer:
+            request.session['correct'] += 1
+        request.session['guessNum'] += 1
+    request.session['prev_guess'] = cur_guess
+    if (request.session['guessNum'] - request.session['correct']) == NUMGUESSES and request.session['correct'] != len(answer):
+        request.session['games'] += 1
+        return render(request, "lose.html")
+    elif request.session['correct'] == len(set(answer)):
+        request.session['games'] += 1
+        request.session['wins'] += 1
+        return render(request, "win.html")
+    else:
+        return render(request, "mainPage.html")
